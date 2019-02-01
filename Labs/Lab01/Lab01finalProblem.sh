712a618caaa838c26017bef64dd7162b26053cdc
Ruiqi-CUB <ruli8003@colorado.edu>
2019-02-01 13:49:15 -0700

    Modified shell script.

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,4 @@
-head -n 1 PredPreyData.csv > New_PredPreyData.csv
-tail -n 10 PredPreyData.csv >> New_PredPreyData.csv
-cut -f 2-4 -d , New_PredPreyData.csv
+head -n 1 PredPreyData.csv > Process_PredPreyData.csv
+tail -n 10 PredPreyData.csv >> Process_PredPreyData.csv
+cut -f 2-4 -d , Process_PredPreyData.csv > New_PredPreyData.csv
+rm Process_PredPreyData.csv
